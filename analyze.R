85ee3a8a77cf355247cf71b1be0b1bb9d4031194
progdrum <brookforestscitech@gmail.com>
2017-10-24 23:06:51 -0600

    Update with some sentiment analysis functions

--- analyze.R
+++ analyze.R
@@ -1,11 +1,13 @@
 library(tidytext)
 library(readr)
 library(dplyr)
+library(purrr)
 library(ggplot2)
 
-# Read in a file to process
+# Read in a file to process (and I should really do some preprocessing here!)
 slurp <- function(filename) {
   file_text <- read_file(filename)
+  return(file_text)
 }
 
 # Basic, initial text processing
@@ -14,6 +16,8 @@ process_text <- function(file_text) {
     unnest_tokens(word, text) %>%
     mutate(word = str_extract(word, "[a-z']+")) %>% 
     anti_join(stop_words)
+  
+  return(tidied)
 }
 
 # Plot word counts (takes pre-processed text from function above)
@@ -23,4 +27,30 @@ word_counts <- function(ptext) {
     ggplot(aes(word, n)) + 
     geom_col() + 
     coord_flip()
+  
+  return(plt)
+}
+
+# Bind sentiment data frames to word tibble
+bind_sentiments <- function(words) {
+  # Use purrr and a parameter to facilitate arbitrary lexicons?
+  nrc_sent <- inner_join(get_sentiments("nrc"), words)
+  afinn_sent <- inner_join(get_sentiments("afinn"), words)
+  
+  # Return as a list
+  return(list(nrc = nrc_sent, afinn = afinn_sent))
+}
+
+# Get NRC sentiment count
+get_nrc_count <- function(words_sent) {
+  return(count(words_sent, sentiment, sort = TRUE))
+}
+
+# Get AFINN sentiment counts
+get_afinn_counts <- function(words_sent) {
+  total <- reduce(words_sent$score, `+`)
+  avg <- mean(words_sent$score)
+  med <- median(words_sent$score)
+  
+  return(list(total = total, avg = avg, med = med))
 }
