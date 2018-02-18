db6378195c11bacc7d733dd465b5e311ed32b269
progdrum <brookforestscitech@gmail.com>
2018-02-18 14:35:10 -0700

    App is rocking a sweet navbarPage now!

--- app.R
+++ app.R
@@ -2,34 +2,53 @@ library(shiny)
 library(shinythemes)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-  theme = shinytheme("sandstone"),
-  sidebarLayout(
-    sidebarPanel(
-      textInput("book", "Enter the name of a book to analyze"),
-      actionButton("gotime", "Go!")
-    ),
-    mainPanel(
-      tabsetPanel(
-        id = "analyses",
-        tabPanel("Top N", plotOutput("plt")),
-        tabPanel("Sentiments", plotOutput("sent")),
-        tabPanel("Topic Analysis", textOutput("soon"))
-      )
-    )
-  )
-)
+ui <- fluidPage(theme = shinytheme("sandstone"),
+                navbarPage(
+                  "Analyzing the Classics",
+                  tabPanel("Counts and Feels",
+                           sidebarLayout(
+                             sidebarPanel(
+                               textInput("book", "Enter the name of a book to analyze"),
+                               actionButton("gotime", "Go!")
+                             ),
+                             mainPanel(tabsetPanel(
+                               id = "analyses",
+                               tabPanel("Top N", plotOutput("plt")),
+                               tabPanel("Sentiments", plotOutput("sent"))
+                             ))
+                           )),
+                  tabPanel("Topics",
+                           sidebarLayout(
+                             sidebarPanel(
+                               textInput("book1", "Enter the name of the first book to compare."),
+                               textInput("book2", "Enter the name of the second book to compare."),
+                               actionButton("topics", "Get Topics")
+                             ),
+                             mainPanel("Topic Analysis", textOutput("soon"))
+                           ))
+                ))
 
 server <- function(input, output) {
   source("analyze.R")
   
-  # Respond to the button and generate the analysis
+  # Generate analyses for "counts and feels"
   observeEvent(input$gotime, {
     txt <- get_book(input$book)
     tidy_book <- process_text(txt)
-    output$plt <- renderPlot({word_counts(tidy_book)})
-    output$sent <- renderPlot({sentiment_counts(tidy_book)})
-    output$soon <- renderText({"Some really cool things are about to happen here!"})
+    output$plt <- renderPlot({
+      word_counts(tidy_book)
+    })
+    output$sent <- renderPlot({
+      sentiment_counts(tidy_book)
+    })
+  })
+  
+  # Generate analysis for "topics"
+  observeEvent(input$topics, {
+    output$soon <-
+      renderText({
+        "Some really cool things are about to happen here!"
+      })
   })
 }
 
